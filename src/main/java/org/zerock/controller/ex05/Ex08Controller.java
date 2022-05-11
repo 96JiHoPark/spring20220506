f6ea8d6c9f182b31fef615976256db9a9048e382
96JiHoPark <99157655+96JiHoPark@users.noreply.github.com>
2022-05-11 12:15:35 +0900

    spring

---
+++ src/main/java/org/zerock/controller/ex05/Ex08Controller.java
@@ -0,0 +1,120 @@
+package org.zerock.controller.ex05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.controller.ex03.Customer;
+import org.zerock.controller.ex03.Employee;
+
+@Controller
+@RequestMapping("ex08")
+public class Ex08Controller {
+	
+	@RequestMapping("sub01")
+	public String method01(HttpServletRequest req)	{
+		
+		req.setAttribute("address", "new york");
+		// prefix : /WEB-INF/views/
+		// suffix : .jsp
+						
+		// full view name : /WEB-INF/views/ex08/sub01.jsp 
+		
+		return "ex08/sub01";
+	}
+	
+	@RequestMapping("sub02")
+	public String method02(Model model) {
+		model.addAttribute("address", "london");
+		
+		return "ex08/sub01";
+	}
+	
+	@RequestMapping("sub03")
+	public String method03(Model model) {
+		model.addAttribute("myName", "짱구");
+		model.addAttribute("myAge", 5);
+		return "ex08/sub03";
+	}
+	
+	@RequestMapping("sub04")
+	public void method04(Model model) {
+		model.addAttribute("foods", new String[] {"apple", "milk", "coffee"});
+	}
+	
+	@RequestMapping("sub05")
+	public void method05(Model model) {
+		List<String> list = new ArrayList<>();
+		list.add("Iron man");
+		list.add("DR.Strange");
+		list.add("Thor");
+		model.addAttribute("heroList", list);
+	}
+	
+	@RequestMapping("sub06")
+	public void method06(Model model) {
+		Map<String, String> map = new HashMap<>();
+		map.put("name", "jin");
+		map.put("song", "butter");
+		map.put("group","bts");
+		
+		model.addAttribute("desc", map);
+	}
+	
+	@RequestMapping("sub07")
+	public void method07(Model model) {
+		Customer cus = new Customer();
+		cus.setAddress("seoul");
+		cus.setAge(30);
+		cus.setName("donald");
+		
+		model.addAttribute("customer", cus);
+	}
+	
+	@RequestMapping("sub08")
+	public void method08(Model model) {
+		Employee emp = new Employee();
+		// email, name, salary 세팅
+		// jsp 완성해서 위에 세팅한 email, name, salary 출력되도록 하기
+		
+		emp.setName("John");
+		emp.setSalary(300);
+		emp.setEmail("John@gmail.com");
+		
+		model.addAttribute("employee", emp);
+	}
+	
+	@RequestMapping("sub09")
+	public String method09(Model model) {
+		Employee emp = new Employee();
+		emp.setEmail("korea@daum.net");
+		emp.setName("sunja");
+		emp.setSalary(70000);
+		
+		model.addAttribute(emp); // attribute이름이 클래스의 이름으로 결정("단, lowerCamelCase형식)
+		//클래스 이름이 Employee 이므로 attribute 이름은 employee 가 된다.		
+		return "ex08/sub08";
+	}
+	
+	@RequestMapping("sub10")
+	public String method10(Model model) {
+		// model.addAttribute(Object)를 사용해서
+		// sub07.jsp에서 고객의 정보가 출력되도록 코드를 작성
+		
+		Customer cus = new Customer();
+		cus.setName("Jiho");
+		cus.setAge(99);
+		cus.setAddress("Seoul");
+		
+		model.addAttribute(cus);
+		
+		return "ex08/sub07";
+	}
+	
+}
